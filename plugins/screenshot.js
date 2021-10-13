8e5c5503b53e39ca7fdf751f28197d2ea885265d
/ SL MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2021-10-13 06:15:02 +0530

    Uᴘᴅᴀᴛᴇ sᴄʀᴇᴇɴsʜᴏᴛ.ᴊs 📨

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -1,9 +1,4 @@
-/* Codded by @phaticusthiccy
-Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
-*/
-
-const Asena = require('../events');
+const AlphaX = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
@@ -16,7 +11,7 @@ const Lang = Language.getString('webss');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'ss ?(.*)', fromMe: true, desc: Lang.SS_DESC}, (async (message, match) => {
+    AlphaX.addCommand({pattern: 'ss ?(.*)', fromMe: true, desc: Lang.SS_DESC}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
@@ -28,7 +23,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'ss ?(.*)', fromMe: false, desc: Lang.SS_DESC}, (async (message, match) => {
+    AlphaX.addCommand({pattern: 'ss ?(.*)', fromMe: false, desc: Lang.SS_DESC}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
@@ -38,4 +33,3 @@ else if (Config.WORKTYPE == 'public') {
 
     }));
 }
-
