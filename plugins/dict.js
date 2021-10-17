45b4baa3901b31b2040bb01d867be6b166193c3c
/ SL MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2021-10-17 08:35:18 +0530

    NEW | .dict  dictation Plugin 🚀

---
+++ plugins/dict.js
@@ -0,0 +1,272 @@
+const AlphaX = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const CON = require ('../config')
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
+
+var DESC == ''
+if (CON.LANG == 'EN') DESC == 'Use it as a dictionary.\nEg: .dict en_US;lead\n For supporting languages send •.lngcode•'
+if (CON.LANG == 'SI') DESC == 'එය ශබ්ද කෝෂයක් ලෙස භාවිතා කරන්න. \nඋදා: .did en_US; hello \n සහය දක්වන භාෂා සඳහා යවන්න •.lngcode•'
+
+var NEED == ''
+if (CON.LANG == 'EN') NEED == '*🔎 Please Enter Word!* \n ```Ex: .dict en_US;hey```'
+if (CON.LANG == 'SI') NEED == '*🔎 කරුණාකර වචනය ඇතුළත් කරන්න!* \n ```උදා: .dict en_US;hey```'
+
+var LOAD == ''
+if (CON.LANG == 'EN') LOAD == '*🔖 Loading ...*'
+if (CON.LANG == 'SI') LOAD == '*🔖 පූරණය වෙමින් කරුණාකර රැඳී සිටින්න ...*'
+
+if (CON.WORKTYPE == 'private') {
+
+AlphaX.addCommand({pattern: 'lngcode', fromMe: true, dontAddCommandList: true}, (async (message, match) => {    
+
+    await message.sendMessage('*Code:* en_US \n *Language:* English (US) \n\n *Code:* hi \n *Language:* Hindi \n\n *Code:* es \n *Language:* Spanish \n\n *Code:* fr \n *Language:* French \n\n *Code:* ja \n *Language:* Japanese \n\n *Code:* ru \n *Language:* Russian \n\n *Code:* en_GB \n *Language:* English (UK) \n\n *Code:* de \n *Language:* German \n\n *Code:* it \n *Language:* Italian \n\n *Code:* ko \n *Language:* Korean \n\n *Code:* pt-BR \n *Language:* Brazilian Portuguese \n\n *Code:* ar \n *Language:* Arabic \n\n *Code:* tr \n *Language:* Turkish \n\n');
+
+}));
+
+AlphaX.addCommand({ pattern: 'dict ?(.*)', fromMe: true, desc: DESC }, async (message, match) => {
+
+    if (!match[1]) return await message.sendMessage(NEED)
+
+ if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        word = split[1];
+        langcode = split[0];
+         }
+	else {
+        word = match[1];
+        langcode = 'en_US';
+        }
+
+    await message.sendMessage(LOAD)
+
+	  
+    await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[0].definitions[0]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	})    
+	    
+	    
+    await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[0].definitions[1]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	 })
+	
+	     await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[1].definitions[0]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	})    
+		     
+	 await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[1].definitions[1]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })    
+	})    
+		 
+		  await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[1].meanings[0].definitions[0]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	    
+
+      })
+      
+  },
+)}
+
+
+if (CON.WORKTYPE == 'public') {
+
+AlphaX.addCommand({pattern: 'lngcode', fromMe: false, dontAddCommandList: true}, (async (message, match) => {    
+
+    await message.sendMessage('*Code:* en_US \n *Language:* English (US) \n\n *Code:* hi \n *Language:* Hindi \n\n *Code:* es \n *Language:* Spanish \n\n *Code:* fr \n *Language:* French \n\n *Code:* ja \n *Language:* Japanese \n\n *Code:* ru \n *Language:* Russian \n\n *Code:* en_GB \n *Language:* English (UK) \n\n *Code:* de \n *Language:* German \n\n *Code:* it \n *Language:* Italian \n\n *Code:* ko \n *Language:* Korean \n\n *Code:* pt-BR \n *Language:* Brazilian Portuguese \n\n *Code:* ar \n *Language:* Arabic \n\n *Code:* tr \n *Language:* Turkish \n\n');
+
+}));
+
+AlphaX.addCommand({ pattern: 'dict ?(.*)', fromMe: false, desc: DESC }, async (message, match) => {
+
+    if (!match[1]) return await message.sendMessage(NEED)
+
+ if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        word = split[1];
+        langcode = split[0];
+         }
+	else {
+        word = match[1];
+        langcode = 'en_US';
+        }
+
+    await message.sendMessage(LOAD)
+
+	  
+    await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[0].definitions[0]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	})    
+	    
+	    
+    await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[0].definitions[1]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	 })
+	
+	     await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[1].definitions[0]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	})    
+		     
+	 await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[0].meanings[1].definitions[1]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })    
+	})    
+		 
+		  await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[1].meanings[0].definitions[0]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+	    
+
+      })
+      
+  },
+)}
