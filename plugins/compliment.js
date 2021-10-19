6d89bb59cc390dbd6fee9da1b1475d7df6840c43
/ SL MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2021-10-19 06:09:03 +0530

    Create compliment.js

---
+++ plugins/compliment.js
@@ -0,0 +1,39 @@
+const AlphaX = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+const axios = require('axios');
+const config = require('../config');
+
+var DESC = ''
+if (config.LANG == 'SI') DESC = 'It sends complimentry sentenses.'
+if (config.LANG == 'EN') DESC = 'එය අනුපූරක වාක්‍ය යවයි.'
+
+if (config.WORKTYPE == 'private') {
+
+    AlphaX.addCommand({pattern: 'compliment$', fromMe: true, desc: DESC}, async (message, match) => {
+ 
+      const url = `https://complimentr.com/api`;
+	  try {
+		  const response = await got(url);
+		  const json = JSON.parse(response.body);
+		  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n\n *compliment : 🤗* ' + '\n🔎 ' + ' ```' + json.compliment + '```\n\n' , MessageType.text);
+	  } catch {
+		  return await message.client.sendMessage(message.jid, '*❌ Error*' , MessageType.text);
+	  }
+  });
+} 
+
+else if (config.WORKTYPE == 'public') {
+
+    AlphaX.addCommand({pattern: 'compliment$', fromMe: true, desc: DESC}, async (message, match) => {
+    
+      const url = `https://complimentr.com/api`;
+	  try {
+		  const response = await got(url);
+		  const json = JSON.parse(response.body);
+		  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n\n *compliment : 🤗* ' + '\n🔎 ' + ' ```' + json.compliment + '```\n\n' , MessageType.text);
+	  } catch {
+		  return await message.client.sendMessage(message.jid, '*❌ Error*' , MessageType.text);
+	  }
+  });
+}
