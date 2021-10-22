db7b585bb6fdb4ba6401808bdeec6e28ee9117e1
/ SL MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2021-10-22 19:10:08 +0530

    New | *.dst* to download any status you replied 📨

---
+++ plugins/down-st.js
@@ -0,0 +1,88 @@
+const AlphaX = require('../events');
+const { MessageType, Mimetype, GroupSettingChange, MessageOptions } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const Config = require('../config');
+const axios = require('axios');
+var DESC = ''
+if (Config.LANG == 'EN') DESC = 'Download status you repled.'
+if (Config.LANG == 'SI') DESC = 'ඔබ රිප්ලයි කල status එක බාගත කර ගැනීමට.'
+
+var SUC = ''
+if (Config.LANG == 'EN') SUC = '*✅ status saved to your log number!*'
+if (Config.LANG == 'SI') SUC = '*✅ status එක ඔබේ ලොග් නම්බරයට යැවුනි!*'
+
+var NEED_REPLY = ''
+if (Config.LANG == 'EN') NEED_REPLY = '*🔎 Reply to any status!*'
+if (Config.LANG == 'SI') NEED_REPLY = '*🔎 කරුනාකර Status එකකට රිප්ලයි කරන්න!*'
+
+
+AlphaX.addCommand({ pattern: 'dst$', fromMe: true, desc: DESC}, (async (message, match) => { 
+const PIC = await axios.get(`${Config.A_PIC}`, {responseType: 'arraybuffer'})
+
+     if (message.reply_message == false) {
+     return await message.client.sendMessage(message.jid,NEED_REPLY, MessageType.text, { contextInfo: { forwardingScore: 49, isForwarded: false }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "🧩 Reply to any status! ❌", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(PIC.data)}}}});
+     }
+
+     else if (message.reply_message.text) {
+            await message.client.sendMessage(
+                message.client.user.jid,
+                 '*🚀 Status From ->* wa.me/' + message.reply_message.jid.split('@')[0] + '\n```TYPE :- text status```\n _• Message >>_ \n' + message.reply_message.text,
+                MessageType.text
+            );
+            await message.client.sendMessage(
+                message.jid,
+                SUC,
+                MessageType.text,
+            );
+        }  
+        else if (message.reply_message.image) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('im.jpg')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('im.jpg'),
+                    MessageType.image,
+                    { caption: '*📨 Status From ->* wa.me/' + message.reply_message.jid.split('@')[0] + '\n```TYPE :- image status```' }
+                );
+                await message.client.sendMessage(
+                    message.jid,
+                    SUC,
+                    MessageType.text,
+                );
+            });
+        }
+        else if (message.reply_message.video) {
+            var location = await message.client.downloadAndSaveMediaMessage({
+                key: {
+                    remoteJid: message.reply_message.jid,
+                    id: message.reply_message.id
+                },
+                message: message.reply_message.data.quotedMessage
+            });
+            ffmpeg(location)
+            .save('vid.mp4')
+            .on('end', async () => {
+                await message.client.sendMessage(
+                    message.client.user.jid,
+                    fs.readFileSync('vid.mp4'),
+                    MessageType.video,
+                    { mimetype: Mimetype.mpeg, caption: '*🏷️ Status From ->* wa.me/' + message.reply_message.jid.split('@')[0] + '\n```TYPE :- video status```' }
+                );
+                await message.client.sendMessage(
+                    message.jid,
+                    SUC,
+                    MessageType.text,
+                );
+            });
+        }
+}));
